1355a8ebad6dc0cd5f31e7a4853f57b15c908bbc
Suman <sumankanwar6438@gmail.com>
2025-03-02 23:13:55 -0800

    modify components

---
+++ src/assets/data.js
@@ -0,0 +1,142 @@
+export const data = [
+    {
+      "title": "Inception",
+      "release_date": "2010-07-16",
+      "rating": 8.8,
+      "description": "A skilled thief, who enters the dreams of others to steal secrets, is given a chance to erase his past crimes by planting an idea into someone's mind.",
+      "image": "one"
+    },
+    {
+      "title": "Interstellar",
+      "release_date": "2014-11-07",
+      "rating": 8.6,
+      "description": "A team of explorers travel through a wormhole in space to ensure humanity's survival.",
+      "image": "two"
+    },
+    {
+      "title": "The Dark Knight",
+      "release_date": "2008-07-18",
+      "rating": 9.0,
+      "description": "Batman faces off against the Joker, a criminal mastermind who brings chaos to Gotham City.",
+      "image": "three"
+    },
+    {
+      "title": "Avatar",
+      "release_date": "2009-12-18",
+      "rating": 7.9,
+      "description": "A paraplegic Marine is sent to the moon Pandora, where he becomes torn between following orders and protecting the new world.",
+      "image": "four"
+    },
+    {
+      "title": "Titanic",
+      "release_date": "1997-12-19",
+      "rating": 7.8,
+      "description": "A love story unfolds aboard the ill-fated Titanic, between a young aristocrat and a struggling artist.",
+      "image": "five"
+    },
+    {
+      "title": "The Matrix",
+      "release_date": "1999-03-31",
+      "rating": 8.7,
+      "description": "A computer hacker learns the shocking truth about reality and his role in the war against its controllers.",
+      "image": "six"
+    },
+    {
+      "title": "Avengers: Endgame",
+      "release_date": "2019-04-26",
+      "rating": 8.4,
+      "description": "The Avengers assemble once more to undo the devastation caused by Thanos and restore balance to the universe.",
+      "image": "seven"
+    },
+    {
+      "title": "Joker",
+      "release_date": "2019-10-04",
+      "rating": 8.4,
+      "description": "A mentally troubled comedian embarks on a downward spiral that leads to the emergence of the Joker.",
+      "image": "eight"
+    },
+    {
+      "title": "Parasite",
+      "release_date": "2019-05-30",
+      "rating": 8.6,
+      "description": "A poor family schemes to infiltrate a wealthy household by posing as unrelated, highly qualified individuals.",
+      "image": "nine"
+    },
+    {
+      "title": "Forrest Gump",
+      "release_date": "1994-07-06",
+      "rating": 8.8,
+      "description": "The story of Forrest Gump, a man with a kind heart and low IQ, who unwittingly influences major historical events.",
+      "image": "ten"
+    },
+    {
+      "title": "The Shawshank Redemption",
+      "release_date": "1994-09-22",
+      "rating": 9.3,
+      "description": "Two imprisoned men bond over years, finding solace and redemption through acts of decency.",
+      "image": "eleven"
+    },
+    {
+      "title": "The Godfather",
+      "release_date": "1972-03-24",
+      "rating": 9.2,
+      "description": "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
+      "image": "twelve"
+    },
+    {
+      "title": "The Lion King",
+      "release_date": "1994-06-24",
+      "rating": 8.5,
+      "description": "A lion cub, Simba, flees his kingdom only to learn the true meaning of responsibility and bravery.",
+      "image": "thirteen"
+    },
+    {
+      "title": "Spider-Man: No Way Home",
+      "release_date": "2021-12-17",
+      "rating": 8.3,
+      "description": "Spider-Man's identity is revealed, bringing chaos to his life and forcing him to seek help from Doctor Strange.",
+      "image": "fourteen"
+    },
+    {
+      "title": "Black Panther",
+      "release_date": "2018-02-16",
+      "rating": 7.3,
+      "description": "T'Challa returns home to Wakanda to take his rightful place as king but faces challenges that test his rule.",
+      "image": "fifteen"
+    },
+    {
+      "title": "The Revenant",
+      "release_date": "2015-12-25",
+      "rating": 8.0,
+      "description": "A frontiersman on a fur trading expedition fights for survival after being left for dead.",
+      "image": "sixteen"
+    },
+    {
+      "title": "Django Unchained",
+      "release_date": "2012-12-25",
+      "rating": 8.4,
+      "description": "A freed slave sets out to rescue his wife from a brutal plantation owner with the help of a bounty hunter.",
+      "image": "seventeen"
+    },
+    {
+      "title": "The Wolf of Wall Street",
+      "release_date": "2013-12-25",
+      "rating": 8.2,
+      "description": "A New York stockbroker engages in corruption and fraud on Wall Street, living a high-flying lifestyle.",
+      "image": "eighteen"
+    },
+    {
+      "title": "Shutter Island",
+      "release_date": "2010-02-19",
+      "rating": 8.2,
+      "description": "A detective investigates the disappearance of a patient from a mental institution on a remote island.",
+      "image": "nineteen"
+    },
+    {
+      "title": "Gladiator",
+      "release_date": "2000-05-05",
+      "rating": 8.5,
+      "description": "A betrayed Roman general seeks vengeance against the corrupt emperor who murdered his family.",
+      "image": "twenty"
+    }
+  ]
